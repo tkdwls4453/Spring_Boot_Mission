671a5339652148f81549d2d963f5997696c7d46a
SANGJIN YOON <tkdwls734@gmail.com>
2022-02-28 22:59:31 +0900

    initial commit

---
+++ 1차/Challenge/firstMission/src/main/java/dev/sangjinyoon/firstMission/FirstMissionApplication.java
@@ -0,0 +1,13 @@
+package dev.sangjinyoon.firstMission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMissionApplication.class, args);
+	}
+
+}
---
+++ 1차/Challenge/firstMission/src/test/java/dev/sangjinyoon/firstMission/FirstMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sangjinyoon.firstMission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
